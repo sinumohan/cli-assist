a9ad8de47c49bd53cd8ae36a426ef6d0f93c8b66
Sinu Mohan <sinumohan777@gmail.com>
2019-05-05 11:16:33 +0530

    Set Full screen

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ const createWindow = async () => {
     width: 800,
     height: 600,
   });
-
+  mainWindow.maximize();
+  mainWindow.setFullScreen(true);
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
